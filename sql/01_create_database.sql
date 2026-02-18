50d91627551adc895b4a9df9b31ec6f43cfb504b
Mr Georges Washington <kodjogeorgesakakpo@gmail.com>
2026-02-18 15:18:31 +0100

    Create and select 'spotify_db' database
    Create database 'spotify_db' if it does not exist and select it for use.

--- sql/01_create_database.sql
+++ sql/01_create_database.sql
@@ -1,1 +1,14 @@
+-- ===============================================
+-- 01_create_database.sql
+-- Objectif : Créer et sélectionner la base de données du projet
+-- ===============================================
+
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'spotify_db')
+BEGIN
+    CREATE DATABASE spotify_db;
+END
+GO
+
+USE spotify_db;
+GO
 
